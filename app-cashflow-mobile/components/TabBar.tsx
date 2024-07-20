d789e903e55765d0f35f212ed1203f878e7be199
MartinezNicolass <martinezz.nnicolas@gmail.com>
2024-07-19 21:24:34 -0300

    working home

---
+++ app-cashflow-mobile/components/TabBar.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function TabBar({ state, descriptors, navigation }: any) {
+  return (
+    <View
+    className="bg-[#110622]  flex flex-row h-[84px] w-full"
+    style={{borderRadius: 20}}
+      /* style={styles.tab} */
+    >
+      {state.routes.map((route: any, index: any) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={index}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    tab: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
